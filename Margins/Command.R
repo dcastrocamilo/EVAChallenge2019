da28ecb77bc518838b48e5c93b15b640e1ba18ee
Daniela Castro Camilo <danielacastrocamilo@Danielas-MacBook-Pro.local>
2019-09-30 20:58:23 +0100

    Minor changes

--- Margins/Command.R
+++ Margins/Command.R
@@ -8,7 +8,7 @@ module load R/3.4.2/intel-2017
 ############
 ### Test ###
 ############
-Rscript SubJobs_eCDF_excmu.R "Replic <- 1" "ncpus <- 1" # the first location
+Rscript SubJobs_eCDF_excmu.R "Replic <- 1" "ncpus <- 1" "jobtime <- '00:45:00'" # the first location
 Rscript SubJobs_eCDF_excthresh.R "Replic <- 1" "ncpus <- 1"
 
 ###########
