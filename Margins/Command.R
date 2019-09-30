0e555068b47668cc202f3760eecebb7382304a57
Daniela Castro-Camilo <danielacastro-camilo@2019-68fef716ad3d-macd.maths.gla.ac.uk>
2019-09-30 11:44:54 +0100

    Submit jobs created with SubJobs_eCDF_excmu.R.

---
+++ Margins/Command.R
@@ -0,0 +1,18 @@
+#########################################################
+##                                                     ##
+## This code launches the jobs in SubJobs_eCDF_excmu.R ##
+##                                                     ##
+#########################################################
+module load R/3.4.2/intel-2017
+
+############
+### Test ###
+############
+Rscript SubJobs_eCDF_excmu.R "Replic <- 1" "ncpus <- 1" # the first location
+Rscript SubJobs_eCDF_excthresh.R "Replic <- 1" "ncpus <- 1"
+
+###########
+### Run ###
+###########
+Rscript SubJobs_eCDF_excmu.R "Replic <- 16703" "ncpus <- 16"
+Rscript SubJobs_eCDF_excthresh.R "Replic <- 16703" "ncpus <- 20"
