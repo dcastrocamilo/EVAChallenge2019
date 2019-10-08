2b9083d330426cbc69b6b5e6b89d04fd3a5268cf
Daniela Castro-Camilo <danielacastro-camilo@2019-68fef716ad3d-macd.maths.gla.ac.uk>
2019-10-08 15:49:27 +0100

    Minor changes

--- Margins/Command.R
+++ Margins/Command.R
@@ -9,10 +9,10 @@ module load R/3.4.2/intel-2017
 ### Test ###
 ############
 Rscript SubJobs_eCDF_excmu.R "Replic <- 1" "ncpus <- 1" "jobtime <- '00:45:00'" # the first location
-Rscript SubJobs_eCDF_excthresh.R "Replic <- 1" "ncpus <- 1"
+Rscript SubJobs_eCDF_excthresh.R "Replic <- 1" "ncpus <- 1" "jobtime <- '00:45:00'" # the first location
 
 ###########
 ### Run ###
 ###########
 Rscript SubJobs_eCDF_excmu.R "Replic <- 16703" "ncpus <- 16"
-Rscript SubJobs_eCDF_excthresh.R "Replic <- 16703" "ncpus <- 20"
+Rscript SubJobs_eCDF_excthresh.R "Replic <- 16703" "ncpus <- 16"
